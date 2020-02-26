0efde281ba695d70a6b1c6c259d44f439222dfd2
Patrick Vinson <j.patrick.vinson@gmail.com>
2020-02-26 09:24:37 -0600

    test controller and SR controller

--- CohesionIB.ServiceRequest/ServiceRequest.Api/Startup.cs
+++ CohesionIB.ServiceRequest/ServiceRequest.Api/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.AspNetCore;
 
 namespace ServiceRequest.Api
 {
@@ -24,6 +26,12 @@ namespace ServiceRequest.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.Debug()
+                .WriteTo.File("logs/log.txt")
+                .CreateLogger();
+
             services.AddControllers();
         }
 
